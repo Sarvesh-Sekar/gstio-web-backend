4c652b03c1862462eeccaa7318880a344ca3755e
sarvesh-sekar <sarveshcs2004@gmail.com>
2025-08-12 08:26:59 +0530

    fix:fixed the domain cors issue

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,13 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://gstio-web.vercel.app", // your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/users", userRoutes);
 
