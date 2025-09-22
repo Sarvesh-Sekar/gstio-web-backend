45400eb74863d3436914cf6b3f654b02b1f0ef33
Sarvesh Sekar <sarveshcs2004@gmail.com>
2025-09-22 14:53:33 +0530

    fix:fixed a typo in cors origin

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://gstio-web.vercel.app",'http://localhost:3000'], // your frontend domain
+    origin: ["https://gstio-web.vercel.app/",'http://localhost:3000'], // your frontend domain
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true
